554a766d2492cca3dd5edc0783da7cfe3bd40008
Kawaiivee <kawaiivee@gmail.com>
2025-02-25 23:04:17 -0600

    add refs for formatted outputs

--- mini-tools-frontend/src/components/WorkItemNamer.vue
+++ mini-tools-frontend/src/components/WorkItemNamer.vue
@@ -13,10 +13,36 @@ enum WorkItemType {
 
 const workItemType = ref("Work");
 const workItemNumber = ref(0);
-const workItemTitle = ref("");
+const workItemName = ref("");
+
+const workItemFormatted = ref("#1234: Feature - Test Work Item");
+const branchFormatted = ref("Feature/1234_Test_Work_Item");
+const pullRequestFormatted = ref("#1234: Feature - Test Work Item");
+
 </script>
 
 
 <template>
-  <v-text-field label="Label"></v-text-field>
+  <v-select
+    label="Work Item Type"
+    :items="[
+      WorkItemType.Feature,
+      WorkItemType.Work,
+      WorkItemType.Bug,
+      WorkItemType.Fault,
+      WorkItemType.Hotfix,
+      WorkItemType.Task,
+      WorkItemType.Spike
+    ]"
+  ></v-select>
+  <v-text-field
+    v-model="workItemName"
+    label="Work Item Name"
+    type="text"
+  ></v-text-field>
+  <button @click="console.log(workItemName)">Format</button>
+  <p>Work Item: {{  }}</p>
+  <p>Branch: {{  }}</p>
+  <p>Pull Request: </p>
+  <p></p>
 </template>
