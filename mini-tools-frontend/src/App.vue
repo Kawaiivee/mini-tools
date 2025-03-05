71d5796d303400704845c9c1eed73cc050dd2771
Kawaiivee <Kawaiivee@gmail.com>
2025-03-05 00:19:45 -0600

    appbar tuff

--- mini-tools-frontend/src/App.vue
+++ mini-tools-frontend/src/App.vue
@@ -4,32 +4,46 @@ import { RouterLink, RouterView } from 'vue-router';
 import router from '@/router';
 
 const drawer = ref(false);
-const routes = router.getRoutes().map(r => r.name);
+const rail = ref(false);
+const currentTool = ref("Home");
 
 </script>
 
 <template>
+<v-app>
   <v-card>
     <v-layout>
+
       <v-app-bar color="primary">
-        <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
         <v-toolbar-title>Mini Tools</v-toolbar-title>
-        <v-spacer></v-spacer>
-        <template v-if="$vuetify.display.mdAndUp">
-          <v-btn icon="mdi-magnify" variant="text"></v-btn>
-
-          <v-btn icon="mdi-filter" variant="text"></v-btn>
-        </template>
-
-        <v-btn icon="mdi-dots-vertical" variant="text"></v-btn>
+        <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
       </v-app-bar>
+
+      <v-navigation-drawer
+        v-model="drawer"
+        class="dynamic-drawer"
+        location="right"
+        temporary>
+        <v-list :drawer="drawer" density="compact" nav>
+          <v-list-item link prepend-icon="mdi-home" value="Home"><v-tooltip activator="parent" text="Home"></v-tooltip></v-list-item>
+          <v-list-item link prepend-icon="mdi-clipboard" value="Work Item Formatter"><v-tooltip activator="parent" text="Work Item Formatter"></v-tooltip></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-main style="height: 100vh;">
+        Main Content Here
+      </v-main>
+
     </v-layout>
   </v-card>
+</v-app>
 </template>
 
-<style scoped>
-div > nav {
-  display: 'flex';
-  flex-direction: 'column';
+<style>
+.dynamic-drawer {
+  width: auto !important;
+  max-width: 300px; /* Prevent it from getting too large */
+  min-width: 100px; /* Prevent it from getting too small */
+  white-space: nowrap;
 }
 </style>
